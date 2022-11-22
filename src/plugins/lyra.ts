945b925de27a2a0cce3cd41bb8e0100db85545b2
samsalvatico <samuele.salvatico.social@gmail.com>
2022-11-22 21:05:06 +0100

    Improved Lyra code

--- src/plugins/lyra.ts
+++ src/plugins/lyra.ts
@@ -1,19 +1,44 @@
-// SHAME CODE
-// This code should be refined
+import { Episode } from "@podverse/podcast-feed-parser";
 import { getSlug } from "./../utils";
 import { getPodcastFeed } from "../utils";
 import fs from "fs/promises";
 import { AstroConfig } from "astro";
-import { create, insertBatch } from "@lyrasearch/lyra";
+import { create, insertBatch, Lyra } from "@lyrasearch/lyra";
 import TurndownService from "turndown";
 
 let cfFolder: AstroConfig;
 
-async function initDB(env: string) {
+type DbEpisode = {
+  title: string,
+  description: string,
+  path: string
+};
+
+type Db = Lyra<{
+  title: "string";
+  description: "string";
+  path: "string";
+}>;
+
+async function initDB(env: string): Promise<void> {
+  const episodes = await getEpisodes(
+    "https://www.spreaker.com/show/4173756/episodes/feed"
+  );
+
+  const db = await populateDb(episodes);
+
+  await writeDbToFile(env, db);
+}
+
+async function getEpisodes(episodesUrl: string): Promise<DbEpisode[]> {
   const { episodes } = await getPodcastFeed(
     "https://www.spreaker.com/show/4173756/episodes/feed"
   );
 
+  return cleanupEpisodes(episodes);
+}
+
+async function populateDb(episodes: DbEpisode[]): Promise<Db> {
   const db = create({
     schema: {
       title: "string",
@@ -21,29 +46,40 @@ async function initDB(env: string) {
       path: "string",
     },
   });
-  const turndownService = new TurndownService();
 
   await insertBatch(
     db,
-    episodes.map((e) => ({
-      title: e.title,
-      description: turndownService
-        .turndown(e.summary)
-        .split("\\")
-        .join("")
-        .replace(/[${-~!"^\+_`\[\]#]/g, "")
-        .split("---")[0],
-      path: getSlug(e),
-    })),
+    episodes,
     { batchSize: 500, language: "italian" }
   );
 
-  const dbFile =
-    env === "dev"
-      ? `${cfFolder.outDir.pathname}/../search.db.json`
-      : `${cfFolder.outDir.pathname}/search.db.json`;
-  await fs.writeFile(dbFile, JSON.stringify(db));
+  return db;
 }
+
+function cleanupEpisodes(episodes: Episode[]): DbEpisode[] {
+  const turndownService = new TurndownService();
+  return episodes.map((e) => ({
+    title: e.title,
+    description: turndownService
+      .turndown(e.summary)
+      .split("\\")
+      .join("")
+      .replace(/[${-~!"^\+_`\[\]#]/g, "")
+      .split("---")[0],
+    path: getSlug(e),
+  }));
+}
+
+const writeDbToFile = (env: string, db: Db): Promise<any> =>
+  fs.writeFile(
+    getDbFilename(env),
+    JSON.stringify(db)
+  );
+
+const getDbFilename = (env: string): string =>
+  env === "dev"
+    ? `${cfFolder.outDir.pathname}/../search.db.json`
+    : `${cfFolder.outDir.pathname}/search.db.json`;
 
 export default () => ({
   name: "GITBAR-ASTRO-LYRA",
